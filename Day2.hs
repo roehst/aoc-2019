868db32b3dc237ecf630b39c04ffa664966db1a4
Rodrigo Stevaux <rodrigo.stevaux@loft.com.br>
2020-11-21 11:08:37 -0300

    Some what improve code

--- Day2.hs
+++ Day2.hs
@@ -1,8 +1,16 @@
+{-# LANGUAGE RecordWildCards #-}
 {-# OPTIONS_GHC -Wall #-}
 
 module Day2 where
 
-type Program = [Int]
+import Control.Monad.State
+
+data Program =
+  Program
+    { pOps :: [Int]
+    , pIp :: Int
+    }
+  deriving (Show)
 
 split :: Eq a => a -> [a] -> [[a]]
 split s l = go s l [] []
@@ -13,38 +21,76 @@ split s l = go s l [] []
       | sep == x = go sep xs [] (reverse buf : results)
       | otherwise = go sep xs (x : buf) results
 
-parse :: String -> Program
-parse source = map (read) $ split ',' source
+parse :: String -> [Int]
+parse source = read <$> split ',' source
+
+update :: Int -> a -> [a] -> [a]
+update _ _ [] = []
+update 0 a (_:xs) = a : xs
+update pos a (x:xs) = x : update (pos - 1) a xs
+
+loadProgram :: IO Program
+loadProgram =
+  Program <$> (update 1 12 <$> update 2 2 <$> parse <$> readFile "Day2.txt") <*>
+  return 0
 
 main :: IO ()
 main = do
-  source <- readFile "Day2.txt"
-  let program = parse source
-  let program' = update (update program 1 12) 2 2
-  print $ eval program' 0
+  program <- loadProgram
+  print program
+  let (a, _) = runState eval program
+  print a
 
-update :: [a] -> Int -> a -> [a]
-update [] _ _ = error "List overflow"
-update (_:xs) 0 v = v : xs
-update (x:xs) n v = x : update xs (n - 1) v
+eval :: State Program Int
+eval = do
+  op <- fetchCurrentInstruction
+  case op of
+    1 -> do
+      addIID 1 2 3
+      moveIp 4
+      eval
+    2 -> do
+      mulIID 1 2 3
+      moveIp 4
+      eval
+    99 -> head <$> gets pOps
+    _ -> error "Unrecognized op code"
 
-eval :: Program -> Int -> Int
-eval program ip =
-  let readIndirect pos = program !! (program !! pos)
-   in let readDirect pos = program !! pos
-       in let op = program !! ip
-           in case op of
-                1 ->
-                  let a = readIndirect (ip + 1)
-                   in let b = readIndirect (ip + 2)
-                       in let dest = readDirect (ip + 3)
-                           in let program' = update program dest (a + b)
-                               in eval program' (ip + 4)
-                2 ->
-                  let a = readIndirect (ip + 1)
-                   in let b = readIndirect (ip + 2)
-                       in let dest = readDirect (ip + 3)
-                           in let program' = update program dest (a * b)
-                               in eval program' (ip + 4)
-                99 -> program !! 0
-                _ -> error $ "Unknown opcode " ++ show op
+moveIp :: Int -> State Program ()
+moveIp offset = do
+  program <- get
+  put program {pIp = pIp program + offset}
+
+addIID :: Int -> Int -> Int -> State Program ()
+addIID a b c = do
+  x <- readIndirect a
+  y <- readIndirect b
+  z <- readDirect c
+  writeDirect z (x + y)
+
+mulIID :: Int -> Int -> Int -> State Program ()
+mulIID a b c = do
+  x <- readIndirect a
+  y <- readIndirect b
+  z <- readDirect c
+  writeDirect z (x * y)
+
+fetchCurrentInstruction :: State Program Int
+fetchCurrentInstruction = do
+  Program {..} <- get
+  return $ pOps !! pIp
+
+readDirect :: Int -> State Program Int
+readDirect offset = do
+  Program {..} <- get
+  return $ pOps !! (pIp + offset)
+
+readIndirect :: Int -> State Program Int
+readIndirect offset = do
+  Program {..} <- get
+  return $ pOps !! (pOps !! (pIp + offset))
+
+writeDirect :: Int -> Int -> State Program ()
+writeDirect dest value = do
+  Program {..} <- get
+  modify $ \s -> s {pOps = update dest value pOps}
