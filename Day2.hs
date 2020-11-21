65719a4483ed35860e8beaca0a0365aec3f01660
Rodrigo Stevaux <rodrigo.stevaux@loft.com.br>
2020-11-21 16:47:01 -0300

    Give ourselves a proper Haskell solution

--- Day2.hs
+++ Day2.hs
@@ -1,96 +1,61 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE RecordWildCards, ScopedTypeVariables,
+  FlexibleInstances #-}
 {-# OPTIONS_GHC -Wall #-}
 
 module Day2 where
 
-import Control.Monad.State
+type MachineL = ([Int], Int)
 
-data Program =
-  Program
-    { pOps :: [Int]
-    , pIp :: Int
-    }
-  deriving (Show)
+class Machine a where
+  getInstructionPointer :: a -> Int
+  setInstructionPointer :: Int -> a -> a
+  createFromSource :: String -> a
+  readDirect :: Int -> a -> Int
+  readIndirect :: Int -> a -> Int
+  writeDirect :: Int -> Int -> a -> a
+  getNextOp :: a -> Int
 
-split :: Eq a => a -> [a] -> [[a]]
-split s l = go s l [] []
-  where
-    go :: Eq a => a -> [a] -> [a] -> [[a]] -> [[a]]
-    go _ [] buf results = reverse $ buf : results
-    go sep (x:xs) buf results
-      | sep == x = go sep xs [] (reverse buf : results)
-      | otherwise = go sep xs (x : buf) results
+instance Machine MachineL where
+  getInstructionPointer (_, i) = i
+  setInstructionPointer i (p, _) = (p, i)
+  createFromSource source = ((map read $ splitOn ',' source), 0)
+  readDirect pos (code, _) = code !! pos
+  readIndirect pos (code, _) = code !! (code !! pos)
+  writeDirect addr val (code, ip) =
+    (take addr code ++ [val] ++ drop (1 + addr) code, ip)
+  getNextOp (code, ip) = code !! ip
 
-parse :: String -> [Int]
-parse source = read <$> split ',' source
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn sep xs = splitOn' sep xs [] []
 
-update :: Int -> a -> [a] -> [a]
-update _ _ [] = []
-update 0 a (_:xs) = a : xs
-update pos a (x:xs) = x : update (pos - 1) a xs
-
-loadProgram :: IO Program
-loadProgram =
-  Program <$> (update 1 12 <$> update 2 2 <$> parse <$> readFile "Day2.txt") <*>
-  return 0
+splitOn' :: Eq a => a -> [a] -> [a] -> [[a]] -> [[a]]
+splitOn' _ [] buf results = reverse $ reverse buf : results
+splitOn' sep (x:xs) buf results
+  | x == sep = splitOn' sep xs [] (reverse buf : results)
+  | otherwise = splitOn' sep xs (x : buf) results
 
 main :: IO ()
 main = do
-  program <- loadProgram
-  print program
-  let (a, _) = runState eval program
-  print a
+  source <- readFile "Day2.txt"
+  let machine = (createFromSource source) :: MachineL
+  print $ run machine
 
-eval :: State Program Int
-eval = do
-  op <- fetchCurrentInstruction
-  case op of
-    1 -> do
-      addIID 1 2 3
-      moveIp 4
-      eval
-    2 -> do
-      mulIID 1 2 3
-      moveIp 4
-      eval
-    99 -> head <$> gets pOps
-    _ -> error "Unrecognized op code"
-
-moveIp :: Int -> State Program ()
-moveIp offset = do
-  program <- get
-  put program {pIp = pIp program + offset}
-
-addIID :: Int -> Int -> Int -> State Program ()
-addIID a b c = do
-  x <- readIndirect a
-  y <- readIndirect b
-  z <- readDirect c
-  writeDirect z (x + y)
-
-mulIID :: Int -> Int -> Int -> State Program ()
-mulIID a b c = do
-  x <- readIndirect a
-  y <- readIndirect b
-  z <- readDirect c
-  writeDirect z (x * y)
-
-fetchCurrentInstruction :: State Program Int
-fetchCurrentInstruction = do
-  Program {..} <- get
-  return $ pOps !! pIp
-
-readDirect :: Int -> State Program Int
-readDirect offset = do
-  Program {..} <- get
-  return $ pOps !! (pIp + offset)
-
-readIndirect :: Int -> State Program Int
-readIndirect offset = do
-  Program {..} <- get
-  return $ pOps !! (pOps !! (pIp + offset))
-
-writeDirect :: Int -> Int -> State Program ()
-writeDirect dest value = do
-  Program {..} <- get
-  modify $ \s -> s {pOps = update dest value pOps}
+run :: Machine a => a -> Int
+run m =
+  let op = getNextOp m
+   in let ip = getInstructionPointer m
+       in case op of
+            1 ->
+              let a = readIndirect (ip + 1) m
+               in let b = readIndirect (ip + 2) m
+                   in let d = readDirect (ip + 3) m
+                       in let m' = writeDirect d (a + b) m
+                           in run (setInstructionPointer (ip + 4) m')
+            2 ->
+              let a = readIndirect (ip + 1) m
+               in let b = readIndirect (ip + 2) m
+                   in let d = readDirect (ip + 3) m
+                       in let m' = writeDirect d (a * b) m
+                           in run (setInstructionPointer (ip + 4) m')
+            99 -> readDirect 0 m
+            _ -> error $ "Undefined instruction: " ++ show op
